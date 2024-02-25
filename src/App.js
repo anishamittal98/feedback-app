59fcf8fc994c6c7a1f6baf70e0c745926500a235
Anisha Mittal <anisha.mittal010@gmail.com>
2024-02-25 23:11:23 +0530

    Header changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react'
-
-
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackData from './data/FeedbackData'
+import FeedackStats from './components/FeedackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './Pages/AboutPage'
 
 function App() {
+
+    const[feedback,setFeedback]=useState(FeedbackData)
+
+    const addFeedback = (newFeedback)=>{
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+    }
+
+    const deleteFeedback=(id)=>{
+        if(window.confirm('Are you sure you want to delete?')){
+            setFeedback(feedback.filter((item)=> item.id !== id))
+        }
+        
+    }
+
   return (
+    <Router>
+    <Header></Header>
     <div className='container'>
-    <h1>Hello from the App Component</h1>
+    <Routes>
+    {/* <Route exact path='/'> */}
+    <FeedbackForm handleAdd={addFeedback}/>
+    <FeedackStats feedback={feedback} />
+    <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+    {/* </Route> */}
+    <Route path='/about' element={<AboutPage/>}></Route>
+    </Routes>
     </div>
+    </Router>
   )
 }
 
